b3e4a66717485e4feeafe5856078038dcd115566
Maksym Sagadin <maxsagadin@gmail.com>
2023-05-17 12:50:19 -0700

    added priority to background image on home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,11 @@ export default function Home() {
         <meta name="description" content="AutoJournal" />
         <meta name='keywords' content='Auto Journal'/>
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#014785" />
       </Head>
       <NavBar />
       <Box height={'100vh'} width={600*3} >
-        <Image src={heroImage} alt='' fill style={{objectFit:"cover"}} quality={100} priority/>
+        <Image src={heroImage} alt='' priority fill style={{objectFit:"cover"}} quality={100} />
         <Typography color={'whitesmoke'}>AutoJournal</Typography>
       </Box>
     </>
