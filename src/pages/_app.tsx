3d7ec14eb06f365dad56f96ecf9416370848ca68
Maksym Sagadin <maxsagadin@gmail.com>
2023-05-17 12:49:58 -0700

    removed font source

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
-import type { AppProps } from 'next/app';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
-
-
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/700.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-
+import { useEffect } from 'react'
+import { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { CacheProvider, EmotionCache } from '@emotion/react'
 import createEmotionCache from '@/utilities/createEmotionCache'
-import lightTheme from '@/styles/themes/lightTheme'
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
+// import lightTheme from '@/styles/themes/lightTheme'
 import darkTheme from '@/styles/themes/darkTheme'
 
 interface MyAppProps extends AppProps {
@@ -18,12 +14,34 @@ interface MyAppProps extends AppProps {
 
 const clientSideEmotionCache = createEmotionCache()
 
-const light = createTheme(lightTheme)
+// const light = createTheme(lightTheme)
 const dark = createTheme(darkTheme)
 
 function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) {
+  const router = useRouter()
+  const protectedRoutes = ['/dashboard']
 
-  return (
+  // Check if the current route is a protected route
+  const isProtectedRoute = protectedRoutes.includes(router.pathname)
+
+  // If the current route is a protected route and the user is not authenticated,
+  // redirect them to the login page
+  useEffect(() => {
+    if (isProtectedRoute) {
+      router.push('/login')
+    }
+  }, [isProtectedRoute, router])
+
+  return isProtectedRoute ? (
+    <SessionProvider session={pageProps.session}>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={dark}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </SessionProvider>
+  ) : (
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={dark}>
         <CssBaseline />
