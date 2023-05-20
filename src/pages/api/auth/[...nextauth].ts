d8edf200d88aca273c135db51a7cf9f7586dec9d
Maksym Sagadin <maxsagadin@gmail.com>
2023-05-20 11:51:00 -0700

    updated NextAuth to use JWT and call connectToDatabase function to connect to client

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,43 +1,48 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { compare } from 'bcryptjs'
-import clientPromise from '@/lib/mongodb'
+import {connectToDatabase} from '@/lib/mongodb'
+
+interface LoginData {
+    email: string
+    password: string
+}
 
 export default NextAuth({
     //Configure JWT
     session: {
         strategy: 'jwt',
     },
+    secret: process.env.SECRET,
+    jwt: {
+        secret: process.env.SECRET,
+    },
     //Specify Provider
     providers: [
         CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                email: { label: "Username", type: "email", placeholder: "name@email.com" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials) {
+            async authorize(credentials: LoginData) {
                 //Connect to DB
-                const client = await clientPromise
+                const client = await connectToDatabase()
                 //Get all the users
                 const users = await client.db().collection('users')
                 //Find user with the email  
                 const result = await users.findOne({email: credentials.email})
                 //Not found - send error res
                 if (!result) {
-                    client.close()
                     throw new Error('No user found with that email')
                 }
                 //Check hashed password with DB password
                 const checkPassword = await compare(credentials.password, result.password)
                 //Incorrect password - send response
                 if (!checkPassword) {
-                    client.close()
                     throw new Error('Password doesnt match')
                 }
                 //Else send success response
-                client.close()
-                return { email: result.email }
+                return {
+                    email: result.email,
+                    name: result.firstName,
+                    image: result.vehicles,
+                }
             },
         }),
     ],
