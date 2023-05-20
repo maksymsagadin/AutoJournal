82636eacd210cf1a29ad080991bdf6d772b71458
Maksym Sagadin <maxsagadin@gmail.com>
2023-05-20 11:41:00 -0700

    wrapped App in SessionProvider and extended App to MyApp to wrap in MUI styling providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,22 @@
 import { useEffect } from 'react'
 import { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import { SessionProvider, useSession } from 'next-auth/react'
+import { Router as NextRouter, useRouter } from 'next/router'
 import { CacheProvider, EmotionCache } from '@emotion/react'
 import createEmotionCache from '@/utilities/createEmotionCache'
 import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
-// import lightTheme from '@/styles/themes/lightTheme'
 import darkTheme from '@/styles/themes/darkTheme'
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache
+  router: NextRouter
 }
 
 const clientSideEmotionCache = createEmotionCache()
-
-// const light = createTheme(lightTheme)
 const dark = createTheme(darkTheme)
 
 function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) {
+  const { data: session, status } = useSession()
   const router = useRouter()
   const protectedRoutes = ['/dashboard']
 
@@ -27,28 +26,25 @@ function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: 
   // If the current route is a protected route and the user is not authenticated,
   // redirect them to the login page
   useEffect(() => {
-    if (isProtectedRoute) {
+    if (isProtectedRoute && status !== 'loading' && !session) {
       router.push('/login')
     }
   }, [isProtectedRoute, router])
 
-  return isProtectedRoute ? (
-    <SessionProvider session={pageProps.session}>
+  return (
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={dark}>
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
       </CacheProvider>
-    </SessionProvider>
-  ) : (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={dark}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
   )
 }
 
-export default MyApp
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <MyApp Component={Component} pageProps={pageProps} router={router} />
+    </SessionProvider>
+  )
+}
