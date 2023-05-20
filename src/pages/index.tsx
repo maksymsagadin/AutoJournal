d544560e34c21fc2fb56ff01329d93cab7f032df
Maksym Sagadin <maxsagadin@gmail.com>
2023-05-20 11:59:17 -0700

    updated inner Box to be a flex box instead of outer because Image 'fill' is relative to it's container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,16 +10,16 @@ export default function Home() {
     <>
       <Head>
         <title>AutoJournal</title>
-        <meta name="description" content="AutoJournal" />
+        <meta name='description' content='AutoJournal' />
         <meta name='keywords' content='Auto Journal'/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <meta name="theme-color" content="#014785" />
+        <meta name="viewport" content='width=device-width, initial-scale=1, viewport-fit=cover' />
+        <meta name='theme-color' content='#014785' />
       </Head>
       <NavBar />
-      <Box height='100vh' maxWidth="100%" overflow="hidden" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-        <Box width={600} mx='auto' >
+      <Box height='100vh' maxWidth='100%' overflow='hidden'>
+        <Box height='100%' display='flex' position='relative' alignItems='center' justifyContent='center'>
           <Image src={heroImage} alt='' priority fill style={{objectFit:'cover', zIndex:'-1'}}  quality={100} />
-          <Typography color={'whitesmoke'} variant='h3' textAlign='center'>AutoJournal</Typography>
+          <Typography color='whitesmoke' variant='h3' textAlign='center'>AutoJournal</Typography>
         </Box>
       </Box>
     </>
