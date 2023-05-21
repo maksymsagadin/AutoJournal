60b85adf85a100c6a88f67f0fb52cd114c21f707
Maksym Sagadin <maxsagadin@gmail.com>
2023-05-20 17:13:18 -0700

    updated to conditionally show login/signup links to profile/signout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
+import { signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
+
 import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 
 import logo from '../../public/autojournalLogo.png'
-import Image from 'next/image'
 
 const rightLink = {
   fontSize: 16,
@@ -12,7 +14,18 @@ const rightLink = {
   ml: 3,
 }
 
-function AppAppBar() {
+const NavBar = () => {
+    const { data: session, status } = useSession()
+    const loading = status === 'loading'
+
+    const handleSignOut = async () => {
+        await signOut()
+        //After signing out, the user will automatically be redirected to the login page once there is no longer a session.
+    }
+
+    if (loading) {
+        return null // Render nothing while loading
+    }
 
     return (
         <AppBar position="sticky">
@@ -28,27 +41,53 @@ function AppAppBar() {
                     <Image src={logo} alt='logo for Auto Journal' priority height={75} width={75} />
                 </Link>
                 <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-                    <Link
-                        color="inherit"
-                        variant="h6"
-                        underline="none"
-                        href="/login"
-                        sx={rightLink}
-                    >
-                        {'Login'}
-                    </Link>
-                    <Link
-                        variant="h6"
-                        underline="none"
-                        href="/signup"
-                        sx={{ ...rightLink, color: 'secondary.main' }}
-                    >
-                        {'Sign Up'}
-                    </Link>
+                    {!session ? (
+                        <>
+                            <Link
+                                color="inherit"
+                                variant="h6"
+                                underline="none"
+                                href="/login"
+                                sx={rightLink}
+                                >
+                                {'Login'}
+                            </Link>
+                            <Link
+                                variant="h6"
+                                underline="none"
+                                href="/signup"
+                                sx={{ ...rightLink, color: 'secondary.main' }}
+                                >
+                                {'Sign Up'}
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link
+                                color="inherit"
+                                variant="h6"
+                                underline="none"
+                                href="/profile"
+                                sx={rightLink}
+                                >
+                                {'Profile'}
+                            </Link>
+                            <Link
+                                variant="h6"
+                                underline="none"
+                                component="button"
+                                onClick={handleSignOut}
+                                sx={{ ...rightLink, color: 'secondary.main' }}
+                                >
+                                {'Sign Out'}
+                            </Link>
+                        </>
+                    )}
+                    
                 </Box>
             </Toolbar>
         </AppBar>
     )
 }
 
-export default AppAppBar
+export default NavBar
