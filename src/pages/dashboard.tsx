ef32fb16e906107b7a3d520770c9965b9906bf94
Maksym Sagadin <maxsagadin@gmail.com>
2023-05-22 17:18:55 -0700

    added error / loading  return statement

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,27 +2,36 @@ import { NextPage, GetServerSideProps } from 'next'
 import type { Session } from 'next-auth'
 import { useSession, getSession } from 'next-auth/react'
 
-import { Box, Typography, Grid } from '@mui/material'
-import AddIcon from '@mui/icons-material/Add'
 import NavBar from '@/components/NavBar'
+
+import { Box, Typography, Grid } from '@mui/material'
+import Downloading from '@mui/icons-material/DownloadingOutlined'
+import AddIcon from '@mui/icons-material/Add'
+
+
 const dashboard: NextPage = () => {
     const { data: session, status } = useSession()
-    const loading = status === 'loading'
 
-    // Access user information and vehicles property
-  const user = session?.user
-  const vehicles  = user?.image
-  console.log(user, 'user')
-  console.log(vehicles, 'vehicles')
+    if (status === 'loading') {
+        return <Box><Downloading /></Box>
+    }
+    if (!session) {
+        return <Box>Error: Not logged in</Box>
+    }
 
-//   const handleAddVehicle= async () = {}
+    const user = session?.user
+    const vehicles  = user?.image
+    // console.log(user, 'user')
+    // console.log(vehicles, 'vehicles')
+
+    // const handleAddVehicle= async () = {}
 
     return (
-        <> = 
+        <>
             <NavBar />
             <Box mt={4} mx={2}>
                 <Typography variant="h4" component="h1" gutterBottom>
-                Welcome, {session?.user?.name}
+                Welcome, {user?.name}
                 </Typography>
                 <Typography variant="h6" component="h2" gutterBottom>
                 You have {vehicles?.length} vehicles
