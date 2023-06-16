a268839905fa60892b8a4c51e88339d54e1d7c08
Maksym Sagadin <maxsagadin@gmail.com>
2023-06-16 12:27:42 -0700

    created POST endpoint for adding vehicles to database

---
+++ src/pages/api/vehicle/add-vehicle.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from '@/lib/mongodb'
+import { authOptions } from '../auth/[...nextauth]'
+import { getServerSession } from 'next-auth/next'
+
+export default async function addVehicleHandler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const newVehicle = req.body
+        const session = await getServerSession(req, res, authOptions)
+
+        if (!session) {
+            res.status(401).json({ message: 'Not authenticated!' })
+            return
+        }
+        const client = await connectToDatabase()
+        const db = client.db().collection('users')
+
+        // Assuming the user's email is stored in the session
+        const user = await db.findOne({ email: session.user?.email })
+
+        if (!user) {
+            res.status(404).json({ message: 'User not found.' })
+            return
+        }
+
+        // Add the new vehicle to the user's vehicles
+        const result = await db.updateOne(
+            { email: user.email },
+            { $push: { vehicles: newVehicle } }
+        )
+        // Find updated user
+        const updatedUser = await db.findOne({ email: session.user?.email })
+        
+        res.status(200).json(updatedUser.vehicles)
+    } else {
+        res.status(500).json({ message: 'Route not valid' })
+    }
+}
