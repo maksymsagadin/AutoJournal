6b49097579f4028b412bb8208011293baf6ac481
Maksym Sagadin <maxsagadin@gmail.com>
2023-08-22 13:33:05 -0700

    made collapsible journal entry card to contain large entries.

--- src/components/JournalEntryCard.tsx
+++ src/components/JournalEntryCard.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import { Button, Card, CardContent, Grid, Typography } from '@mui/material'
+import { Card, CardContent, Typography, IconButton, Collapse, Grid, Button } from '@mui/material'
 import { JournalEntry } from '@/utils/types'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 import EditIcon from '@mui/icons-material/Edit'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditJournalEntry from './Forms/EditJournalEntry'
@@ -12,8 +14,10 @@ interface JournalEntryCardProps {
 }
 
 const JournalEntryCard: React.FC<JournalEntryCardProps> = ({ entry, onEdit, onDelete }) => {
+    const [expanded, setExpanded] = useState(false)
     const [isEditing, setIsEditing] = useState(false)
     const [isDeleting, setIsDeleting] = useState(false)
+
     const handleEdit = (updatedEntry: JournalEntry) => {
         onEdit(updatedEntry)
         setIsEditing(false)
@@ -22,6 +26,10 @@ const JournalEntryCard: React.FC<JournalEntryCardProps> = ({ entry, onEdit, onDe
     const handleDelete = (entryToDelete: JournalEntry) => {
         onDelete(entryToDelete)
     }
+
+    const handleExpandClick = () => {
+        setExpanded(prevState => !prevState)
+      }
 
     if (isEditing) {
         return (
@@ -54,24 +62,35 @@ const JournalEntryCard: React.FC<JournalEntryCardProps> = ({ entry, onEdit, onDe
                     </Grid>
                     <Grid item xs={5} sm={5}>
                         <Typography variant='overline' fontSize={'1.2rem'} component='p'>
-                            {entry.spent ? `💸: ${entry.spent}`: '💸: ~'}
+                            {entry.spent ? `💸: ${entry.spent}` : '💸: ~'}
                         </Typography>
                     </Grid>
-                    {entry.parts && <Grid item xs={12} >
-                        <Typography variant='overline' fontSize={'1.1rem'} component='p'>
-                            🔩: {entry.parts}
-                        </Typography>
-                    </Grid>}
-                    {entry.tools && <Grid item xs={12} >
-                        <Typography variant='overline' fontSize={'1.1rem'} component='p'>
-                            🔧: {entry.tools}
-                        </Typography>
-                    </Grid>}
                     <Grid item xs={12}>
+                    {!expanded ? (
+                        <Typography variant='overline' fontSize={'1rem'} component='p'>
+                            📝 : {entry.notes.substring(0, 75)}...
+                        </Typography>
+                    ) : (
                         <Typography variant='overline' fontSize={'1rem'} component='p'>
                             📝 : {entry.notes}
                         </Typography>
-                    </Grid>
+                    )}
+                    <IconButton size="small" onClick={handleExpandClick}>
+                        {expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                    </IconButton>
+                </Grid>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit sx={{ width: '100%', padding: '0 1rem' }}>
+                        {entry.parts && <Grid item xs={12}>
+                            <Typography variant='overline' fontSize={'1.1rem'} component='p'>
+                                🔩: {entry.parts}
+                            </Typography>
+                        </Grid>}
+                        {entry.tools && <Grid item xs={12}>
+                            <Typography variant='overline' fontSize={'1.1rem'} component='p'>
+                                🔧: {entry.tools}
+                            </Typography>
+                        </Grid>}
+                    </Collapse>
                     <Grid item textAlign={'center'} xs={12}>
                         <Button endIcon={<EditIcon />} variant='contained' color='primary' onClick={() => setIsEditing(true)}>
                             <Typography variant='overline'>Edit</Typography>
