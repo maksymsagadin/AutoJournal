0045fb5e1570e037141abb6ef2fbfb790586e2ad
Maksym Sagadin <maxsagadin@gmail.com>
2023-08-23 13:42:20 -0700

    added tooltips for each input field when adding new journal entries

--- src/components/Forms/AddJournalEntry.tsx
+++ src/components/Forms/AddJournalEntry.tsx
@@ -3,7 +3,7 @@ import { useSession } from 'next-auth/react'
 import { v4 as uuidv4 } from 'uuid'
 import { JournalEntry, Vehicle } from '@/utils/types'
 
-import { Box, TextField, Checkbox, FormControlLabel, Button, Paper, Typography, Grid, InputAdornment, MenuItem } from '@mui/material'
+import { Box, TextField, Checkbox, FormControlLabel, Button, Paper, Typography, Grid, InputAdornment, MenuItem, Tooltip } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import CarRepairIcon from '@mui/icons-material/CarRepair' // service
 import SpeedIcon from '@mui/icons-material/Speed' // odometer
@@ -82,118 +82,134 @@ const AddJournalEntry: React.FC<AddJournalEntryProps> = ({ vehicle, onAddEntry }
                     <Box component='form' onSubmit={handleSubmit}>
                         <Grid container spacing={2}>
                             <Grid item xs={6} sm={7}>
-                                <TextField
-                                    select
-                                    label='Service'
-                                    name='service'
-                                    type='text'
-                                    margin='dense'
-                                    fullWidth
-                                    required
-                                    defaultValue='Upgrade'
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position='start'><CarRepairIcon /></InputAdornment>,
-                                    }}
-                                >
-                                    <MenuItem value={'Service'}>Service</MenuItem>
-                                    <MenuItem value={'Upgrade'}>Upgrade</MenuItem>
-                                    <MenuItem value={'Repair'}>Repair</MenuItem>
-                                    <MenuItem value={'Other'}>Other</MenuItem>
-                                </TextField>
+                                <Tooltip title="Enter the type of service performed" placement='top' enterTouchDelay={50}>
+                                    <TextField
+                                        select
+                                        label='Service'
+                                        name='service'
+                                        type='text'
+                                        margin='dense'
+                                        fullWidth
+                                        required
+                                        defaultValue='Upgrade'
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position='start'><CarRepairIcon /></InputAdornment>,
+                                        }}
+                                    >
+                                        <MenuItem value={'Service'}>Service</MenuItem>
+                                        <MenuItem value={'Upgrade'}>Upgrade</MenuItem>
+                                        <MenuItem value={'Repair'}>Repair</MenuItem>
+                                        <MenuItem value={'Other'}>Other</MenuItem>
+                                    </TextField>
+                                </Tooltip>
                             </Grid>
                             <Grid item xs={6} sm={5}>
-                                <TextField
-                                    label='Mileage'
-                                    name='mileage'
-                                    type='number'
-                                    defaultValue={vehicle.mileage}
-                                    margin='dense'
-                                    fullWidth
-                                    required
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position='start'><SpeedIcon /></InputAdornment>,
-                                    }}
-                                />
+                                <Tooltip title="Enter the mileage of the vehicle during this service" placement='top' enterTouchDelay={50}>
+                                    <TextField
+                                        label='Mileage'
+                                        name='mileage'
+                                        type='number'
+                                        defaultValue={vehicle.mileage}
+                                        margin='dense'
+                                        fullWidth
+                                        required
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position='start'><SpeedIcon /></InputAdornment>,
+                                        }}
+                                    />
+                                </Tooltip>
                             </Grid>
                             <Grid item xs={7} sm={7}>
-                                <TextField
-                                    label='Date'
-                                    name='date'
-                                    type='date'
-                                    defaultValue={new Date().toLocaleDateString('en-CA')} // Current date
-                                    margin='dense'
-                                    fullWidth
-                                    required
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position='start'><EventNoteIcon /></InputAdornment>,
-                                    }}
-                                />
+                                <Tooltip title="Enter the date this service was performed" placement='top' enterTouchDelay={50}>
+                                    <TextField
+                                        label='Date'
+                                        name='date'
+                                        type='date'
+                                        defaultValue={new Date().toLocaleDateString('en-CA')} // Current date
+                                        margin='dense'
+                                        fullWidth
+                                        required
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position='start'><EventNoteIcon /></InputAdornment>,
+                                        }}
+                                    />
+                                </Tooltip>
                             </Grid>
                             <Grid item xs={5} sm={5}>
-                                <TextField
-                                    label='Spent'
-                                    name='spent'
-                                    type='number'
-                                    margin='dense'
-                                    fullWidth
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position='start'><AttachMoneyIcon /></InputAdornment>,
-                                    }}
-                                />
+                                <Tooltip title="How much was spent overall?" placement='top' enterTouchDelay={50}>
+                                    <TextField
+                                        label='Spent'
+                                        name='spent'
+                                        type='number'
+                                        margin='dense'
+                                        fullWidth
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position='start'><AttachMoneyIcon /></InputAdornment>,
+                                        }}
+                                    />
+                                </Tooltip>
                             </Grid>
                             <Grid item xs={12}>
-                                <TextField
-                                    label='Notes'
-                                    name='notes'
-                                    type='text'
-                                    margin='dense'
-                                    fullWidth
-                                    required
-                                    multiline
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position='start'><DescriptionIcon /></InputAdornment>,
-                                    }}
-                                />
+                            <Tooltip title="What did you do?" placement='top' enterTouchDelay={50}>
+                                    <TextField
+                                        label='Notes'
+                                        name='notes'
+                                        type='text'
+                                        margin='dense'
+                                        fullWidth
+                                        required
+                                        multiline
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position='start'><DescriptionIcon /></InputAdornment>,
+                                        }}
+                                    />
+                                </Tooltip>
                             </Grid>
                             <Grid item xs={12}>
-                                <TextField
-                                    label='Parts'
-                                    name='parts'
-                                    type='text'
-                                    margin='dense'
-                                    fullWidth
-                                    multiline
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position='start'><SportsIcon /></InputAdornment>,
-                                    }}
-                                />
+                                <Tooltip title="What part(s) did you install or replace?" placement='top' enterTouchDelay={50}>
+                                    <TextField
+                                        label='Parts'
+                                        name='parts'
+                                        type='text'
+                                        margin='dense'
+                                        fullWidth
+                                        multiline
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position='start'><SportsIcon /></InputAdornment>,
+                                        }}
+                                    />
+                                </Tooltip>
                             </Grid>
                             <Grid item xs={12}>
-                                <TextField
-                                    label='Tools'
-                                    name='tools'
-                                    type='text'
-                                    margin='dense'
-                                    fullWidth
-                                    multiline
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position='start'><ConstructionIcon /></InputAdornment>,
-                                    }}
-                                />
+                                <Tooltip title="What tool(s) did you need?" placement='top' enterTouchDelay={50}>
+                                    <TextField
+                                        label='Tools'
+                                        name='tools'
+                                        type='text'
+                                        margin='dense'
+                                        fullWidth
+                                        multiline
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position='start'><ConstructionIcon /></InputAdornment>,
+                                        }}
+                                    />
+                                </Tooltip>
                             </Grid>
                             <Grid item>
-                                <FormControlLabel
-                                    label='Future Entry?'
-                                    control={
-                                        <Checkbox
-                                            name='future'
-                                            color='primary'
-                                        />
-                                    }
-                                />
+                                <Tooltip title="Future service or already completed?" placement='right' enterTouchDelay={50}>
+                                    <FormControlLabel
+                                        label='Future Entry?'
+                                        control={
+                                            <Checkbox
+                                                name='future'
+                                                color='primary'
+                                            />
+                                        }
+                                    />
+                                </Tooltip>
                             </Grid>
                             <Grid item xs={12}>
                                 <Button type='submit' variant='contained' color='primary' sx={{ my: 1 }}>
